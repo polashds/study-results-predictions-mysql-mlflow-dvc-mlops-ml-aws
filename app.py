a525f752885cacc8d628a568c5f3b15fb5f5bb68
Polash <polashtechai@gmail.com>
2025-08-17 19:21:09 +0600

    logger exception setting

---
+++ app.py
@@ -0,0 +1,18 @@
+# from src.mlproject.logger import logging
+
+# if __name__=="__main__":
+#     logging.info("The execution has started")
+
+# from src.mlproject.exception import CustomException
+# from src.mlproject.logger import logging
+# import sys
+
+# if __name__=="__main__":
+#     logging.info("The execution has started")
+
+#     try:
+#         a=1/0
+
+#     except Exception as e:
+#         logging.info("Custom Exception")
+#         raise CustomException(e,sys)
